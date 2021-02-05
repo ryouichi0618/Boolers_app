f779755913e07b3048269049b88388b60a2758d8
ryouichi0618 <ec2-user@ip-172-31-51-47.ec2.internal>
2021-02-05 00:26:06 +0000

    [Add]　編集、削除、サクセスメッセージの追加、レイアウトの修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,19 +3,44 @@ class BooksController < ApplicationController
      @book = Book.new
      @books = Book.all
   end
+  
+   def create
+    book = Book.new(book_params)
+    if book.save
+       flash[:notice] = "Book was successfully created."
+       redirect_to book_path(book)
+    else
+      @books=Book.all
+      @book = book
+      render action: :index
+    end
+   end
 
   def show
     @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
   
-  def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to books_path
+  def update
+    book = Book.find(params[:id])
+    if book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+      redirect_to book_path(book)
+    else
+      @book = book
+      render action: :edit
+    end
   end
+ 
+ def destroy
+   book = Book.find(params[:id])
+   book.destroy
+   flash[:notice] = "Book was successfully destroyed."
+   redirect_to books_path
+ end
   
   private
   
